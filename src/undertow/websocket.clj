ec9bfa79bf44e58c67e49e1c91d803b2c8a7859c
serioga <github@sergey.trofimov.name>
2022-12-06 21:40:01 +0100

    Websockets, pass context in callback handlers

--- src/undertow/websocket.clj
+++ src/undertow/websocket.clj
@@ -7,25 +7,26 @@
 ;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 
 (defn channel-listener
-  [callbacks]
-  (WebSocketChannelListener. callbacks))
+  {:arglists '([{:keys [on-message, on-close, on-error]}])}
+  [handlers]
+  (WebSocketChannelListener. handlers))
 
 (defn websocket-callback
-  {:arglists '([{:keys [on-complete, on-error]}])}
+  {:arglists '([{:keys [on-complete, on-error, context]}])}
   ^WebSocketCallback
   [handlers]
   (reify WebSocketCallback
     (complete
       [_ channel context]
       (when-let [on-complete (:on-complete handlers)]
-        (on-complete {:channel channel, :context context})))
+        (on-complete {:channel channel, :context (:context handlers context)})))
     (onError
       [_ channel context throwable]
       (when-let [on-error (:on-error handlers)]
-        (on-error {:channel channel, :context context, :error throwable})))))
+        (on-error {:channel channel, :context (:context handlers context), :error throwable})))))
 
 ;; TODO: docstrings
-(defprotocol WebSocketOps
+(defprotocol WebSocketSend
   (send-text
     [message channel opts]
     "message channel {:keys [on-complete, on-error, context, timeout]}")
@@ -35,13 +36,11 @@
   (send-close [message channel opts])
   (send-close-blocking [message channel]))
 
-(extend-protocol WebSocketOps String
+(extend-protocol WebSocketSend String
   (send-text
     [message channel opts]
     (WebSockets/sendText message, ^WebSocketChannel channel
-                         (websocket-callback opts)
-                         (:context opts)
-                         ^long (:timeout opts -1)))
+                         (websocket-callback opts) ^long (:timeout opts -1)))
   (send-text-blocking
     [message channel]
     (WebSockets/sendTextBlocking message, ^WebSocketChannel channel)))
