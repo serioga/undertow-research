fdb92f3290b48eb89902be4205549de63d0458ab
Sergey Trofimov <sergey@trofimov.name>
2022-12-09 17:00:29 +0100

    Set :java-source-paths

--- project.clj
+++ project.clj
@@ -16,5 +16,7 @@
                  [org.clojure/tools.logging "1.2.4"]
                  [org.slf4j/slf4j-api "1.7.36" :upgrade false]]
 
+  :java-source-paths ["src"]
+
   :profiles {:dev {:dependencies [[luminus/ring-undertow-adapter "1.2.9"]
                                   [org.immutant/web "2.1.10"]]}})
