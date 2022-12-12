38e2b465b138002637de8d3cb0b660597c4bd9f2
Sergey Trofimov <sergey@trofimov.name>
2022-12-12 16:56:24 +0100

    Upgrade zizzmap

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [ring/ring-defaults "0.3.4"]
 
                  ;;; Utils
-                 [com.github.strojure/zizzmap "1.0.20"]
+                 [com.github.strojure/zizzmap "1.1.26"]
 
                  ;;; Logging
                  [ch.qos.logback/logback-classic "1.2.11" :upgrade false]
