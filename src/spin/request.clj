7c8e6a4856bf5b1bf1138011e34208f75c52067d
Sergey Trofimov <sergey@trofimov.name>
2023-04-26 14:30:50 +0200

    (refactor spin): rename `query-string` and `header-seq`

--- src/spin/request.clj
+++ src/spin/request.clj
@@ -8,19 +8,18 @@
 
 (defprotocol ISpinRequest
   (internal [_])
-  (as-map [_])
   (server-port [_])
   (server-name [_])
   (remote-addr [_])
   (uri [_])
-  (query-string [_])
+  (query [_])
   (scheme [_])
   (method [_])
   (body ^java.io.InputStream [_])
   (path-info [_])
   (protocol [_])
-  (header [_ name])
-  (header-seq [_ name]))
+  (header [_ name] "Returns first value of the header `name`.")
+  (header* [_ name] "Returns seq of all values of the header `name`."))
 
 (defn method-get? [req]
   ;; TODO: inline function
@@ -34,20 +33,19 @@
 
 (extend-protocol ISpinRequest ILookup
   (internal [m] m)
-  (as-map [m] m)
-  (server-port [m],,, (.valAt m :server-port))
-  (server-name [m],,, (.valAt m :server-name))
-  (remote-addr [m],,, (.valAt m :remote-addr))
-  (uri [m],,,,,,,,,,, (.valAt m :uri))
-  (query-string [m],, (.valAt m :query-string))
-  (scheme [m],,,,,,,, (.valAt m :scheme))
-  (method [m],,,,,,,, (.valAt m :request-method))
-  (body [m],,,,,,,,,, (.valAt m :body))
-  (path-info [m],,,,, (.valAt m :path-info))
-  (protocol [m],,,,,, (.valAt m :protocol))
+  (server-port [m] (.valAt m :server-port))
+  (server-name [m] (.valAt m :server-name))
+  (remote-addr [m] (.valAt m :remote-addr))
+  (uri [m],,,,,,,, (.valAt m :uri))
+  (query [m],,,,,, (.valAt m :query-string))
+  (scheme [m],,,,, (.valAt m :scheme))
+  (method [m],,,,, (.valAt m :request-method))
+  (body [m],,,,,,, (.valAt m :body))
+  (path-info [m],, (.valAt m :path-info))
+  (protocol [m],,, (.valAt m :protocol))
   (header
     [m h] (get (.valAt m :headers) (string/lower-case h)))
-  (header-seq
+  (header*
     [m h] (some-> (header m h) (string/split #",\s*"))))
 
 (comment
@@ -58,8 +56,8 @@
   (-h (string/lower-case "Content-Type"))
   (-h (string/lower-case "content-type"))
   (header {:headers -h} "content-type")
-  (header-seq {:headers -h} "content-type")
-  (header-seq {:headers -h} "x-test-seq")
+  (header* {:headers -h} "content-type")
+  (header* {:headers -h} "x-test-seq")
   (:headers {:headers {}})
   )
 
