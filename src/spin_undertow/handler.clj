1bbf5070c43de19f606751fca2d1b73df226a551
Sergey Trofimov <sergey@trofimov.name>
2023-04-30 19:15:32 +0200

    (refactor spin): separate ns spin-undertow.response

--- src/spin_undertow/handler.clj
+++ src/spin_undertow/handler.clj
@@ -1,56 +1,13 @@
 (ns spin-undertow.handler
   (:require [spin-undertow.request :as request]
+            [spin-undertow.response :as response]
             [spin.handler :as handler]
             [undertow.api.exchange :as exchange])
   (:import (clojure.lang IPersistentMap)
            (io.undertow.server HttpHandler HttpServerExchange)
-           (io.undertow.util HeaderMap HttpString SameThreadExecutor)
-           (java.nio.charset Charset)
-           (java.util Collection)))
+           (io.undertow.util SameThreadExecutor)))
 
 (set! *warn-on-reflection* true)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn- put-headers!
-  [exchange headers]
-  (reduce (fn [^HeaderMap hm [k v]]
-            (cond (sequential? v)
-                  (-> hm (.putAll (HttpString. (str k))
-                                  ^Collection (map str v)))
-                  (some? v)
-                  (-> hm (.put (HttpString. (str k)) (str v)))
-                  :else
-                  (-> hm (.remove (HttpString. (str k))))))
-          (.getResponseHeaders ^HttpServerExchange exchange)
-          headers))
-
-(defprotocol SpinResponse
-  (handle-response [response exchange]))
-
-(defprotocol SpinResponseBody
-  (handle-response-body [body exchange]))
-
-(extend-protocol SpinResponse IPersistentMap
-  (handle-response
-    [response ^HttpServerExchange exchange]
-    (when-some [headers,, (.valAt response :headers)] (doto exchange (put-headers! headers)))
-    (when-some [status,,, (.valAt response :status)], (.setStatusCode exchange status))
-    ;(when-some [session (.entryAt response :session)] (doto exchange (session/update-values (val session))))
-    (when-some [body,,,,, (.valAt response :body)],,, (handle-response-body body exchange))
-    nil))
-
-;; Allow just a body in response
-(extend-protocol SpinResponse Object
-  (handle-response
-    [object, exchange]
-    (handle-response-body object exchange)))
-
-(extend-protocol SpinResponseBody String
-  (handle-response-body
-    [string, ^HttpServerExchange exchange]
-    (-> (.getResponseSender exchange)
-        (.send string (Charset/forName (.getResponseCharset exchange))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -61,9 +18,9 @@
     ;; Add prepending response headers from context.
     (some->> (.valAt context :response/headers)
              ;; TODO: not-empty here?
-             (put-headers! exchange))
+             (response/put-headers! exchange))
     (when-some [status (.valAt context :response/status)] (.setStatusCode exchange status))
-    (some-> (.valAt context :response) (handle-response exchange))
+    (some-> (.valAt context :response) (response/handle-response exchange))
 
     #_(let [end-time (System/nanoTime)]
         #p (- end-time (:start-time context))))
---
+++ src/spin_undertow/response.clj
@@ -0,0 +1,54 @@
+(ns spin-undertow.response
+  (:import (clojure.lang IPersistentMap)
+           (io.undertow.server HttpServerExchange)
+           (io.undertow.util HeaderMap HttpString)
+           (java.nio.charset Charset)
+           (java.util Collection)))
+
+(set! *warn-on-reflection* true)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn put-headers!
+  [exchange headers]
+  (reduce (fn [^HeaderMap hm [k v]]
+            (cond (sequential? v)
+                  (-> hm (.putAll (HttpString. (str k))
+                                  ^Collection (map str v)))
+                  (some? v)
+                  (-> hm (.put (HttpString. (str k)) (str v)))
+                  :else
+                  (-> hm (.remove (HttpString. (str k))))))
+          (.getResponseHeaders ^HttpServerExchange exchange)
+          headers))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defprotocol SpinResponse
+  (handle-response [response exchange]))
+
+(defprotocol SpinResponseBody
+  (handle-response-body [body exchange]))
+
+(extend-protocol SpinResponse IPersistentMap
+  (handle-response
+    [response ^HttpServerExchange exchange]
+    (when-some [headers,, (.valAt response :headers)] (doto exchange (put-headers! headers)))
+    (when-some [status,,, (.valAt response :status)], (.setStatusCode exchange status))
+    ;(when-some [session (.entryAt response :session)] (doto exchange (session/update-values (val session))))
+    (when-some [body,,,,, (.valAt response :body)],,, (handle-response-body body exchange))
+    nil))
+
+;; Allow just a body in response
+(extend-protocol SpinResponse Object
+  (handle-response
+    [object, exchange]
+    (handle-response-body object exchange)))
+
+(extend-protocol SpinResponseBody String
+  (handle-response-body
+    [string, ^HttpServerExchange exchange]
+    (-> (.getResponseSender exchange)
+        (.send string (Charset/forName (.getResponseCharset exchange))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
