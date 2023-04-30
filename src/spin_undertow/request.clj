bec3c59865601f4cb1e6e8a4f170341b146f5a37
Sergey Trofimov <sergey@trofimov.name>
2023-04-30 19:09:11 +0200

    (style): reformat spin-undertow.request

--- src/spin_undertow/request.clj
+++ src/spin_undertow/request.clj
@@ -10,39 +10,48 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn -server-exchange
-  [^HttpServerExchange e _] e)
+  [^HttpServerExchange e _]
+  e)
 
 (defn -server-port
-  [^HttpServerExchange e _] (.getPort (.getDestinationAddress e)))
+  [^HttpServerExchange e _]
+  (.getPort (.getDestinationAddress e)))
 
 (defn -server-name
-  [^HttpServerExchange e _] (.getHostName e))
+  [^HttpServerExchange e _]
+  (.getHostName e))
 
 (defn -remote-addr
-  [^HttpServerExchange e _] (.getHostAddress (.getAddress (.getSourceAddress e))))
+  [^HttpServerExchange e _]
+  (.getHostAddress (.getAddress (.getSourceAddress e))))
 
 (defn -uri
-  [^HttpServerExchange e _] (.getRequestURI e))
+  [^HttpServerExchange e _]
+  (.getRequestURI e))
 
 (defn -query-string
-  [^HttpServerExchange e _] (as-> (.getQueryString e) s
-                                  (when-not (.isEmpty s) s)))
+  [^HttpServerExchange e _]
+  (as-> (.getQueryString e) s
+        (when-not (.isEmpty s) s)))
 
 (defn -scheme
-  [^HttpServerExchange e _] (as-> (.getRequestScheme e) s
-                                  (case s "http" :http "https" :https
-                                          (keyword (.toLowerCase ^String s)))))
+  [^HttpServerExchange e _]
+  (as-> (.getRequestScheme e) s
+        (case s "http" :http "https" :https
+                (keyword (.toLowerCase ^String s)))))
 
 (defn -method
-  ([^HttpServerExchange e _] (as-> (.toString (.getRequestMethod e)) s
-                                   (case s "GET" :get "POST" :post "PUT" :put
-                                           "DELETE" :delete "HEAD" :head "OPTIONS" :options
-                                           (keyword (.toLowerCase ^String s)))))
+  ([^HttpServerExchange e _]
+   (as-> (.toString (.getRequestMethod e)) s
+         (case s "GET" :get "POST" :post "PUT" :put
+                 "DELETE" :delete "HEAD" :head "OPTIONS" :options
+                 (keyword (.toLowerCase ^String s)))))
   ([e _ x] (.equals ^Object (-method e _) x)))
 
 ;; TODO: dispatch blocking when work with input stream
 (defn -body
-  [^HttpServerExchange e _] (exchange/get-input-stream e))
+  [^HttpServerExchange e _]
+  (exchange/get-input-stream e))
 
 (defn -header
   ([^HttpServerExchange e _ x]
@@ -64,15 +73,17 @@
      (-query-param e _ x))))
 
 (defn -cookie
-  [^HttpServerExchange e _ x] (some-> (.getRequestCookie e x) (.getValue)))
+  [^HttpServerExchange e _ x]
+  (some-> (.getRequestCookie e x) (.getValue)))
 
 (defn -cookie-info
-  [^HttpServerExchange e _ x] (when-let [c (.getRequestCookie e x)]
-                                ;; TODO: cookie map fields
-                                ;; TODO: skip empty fields?
-                                {:name (.getName c)
-                                 :value (.getValue c)
-                                 :path (.getPath c)}))
+  [^HttpServerExchange e _ x]
+  (when-let [c (.getRequestCookie e x)]
+    ;; TODO: cookie map fields
+    ;; TODO: skip empty fields?
+    {:name (.getName c)
+     :value (.getValue c)
+     :path (.getPath c)}))
 
 (defonce ^{:doc ""}
          request-state-attachment-key (AttachmentKey/create Object))
