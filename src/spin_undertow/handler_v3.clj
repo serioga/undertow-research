7f9854c35a6de7208a5cb1e39619adf439e5e24f
Sergey Trofimov <sergey@trofimov.name>
2023-09-26 16:11:21 +0200

    (feat spin v3): test undertow server

---
+++ src/spin_undertow/handler_v3.clj
@@ -0,0 +1,52 @@
+(ns spin-undertow.handler-v3
+  (:require [spin-undertow.request :as request]
+            [spin-undertow.response :as response]
+            [spin.handler-v3 :as handler]
+            [spin.impl-v3.adapter :as adapter]
+            [undertow.api.exchange :as exchange])
+  (:import (clojure.lang IPersistentMap)
+           (io.undertow.server HttpHandler HttpServerExchange)
+           (io.undertow.util SameThreadExecutor)))
+
+(set! *warn-on-reflection* true)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- handle-result-context
+  [^IPersistentMap context, ^HttpServerExchange e]
+  ;; TODO: apply prepending context transformations
+  (when context
+    ;; Add prepending response headers from context.
+    (some->> (.valAt context :response/headers)
+             ;; TODO: not-empty here?
+             (response/put-headers! e))
+    (when-some [status (.valAt context :response/status)] (.setStatusCode e status))
+    (some-> (.valAt context :response) (response/handle-response e))
+
+    #_(let [end-time (System/nanoTime)]
+        #p (- end-time (:start-time context))))
+
+  (.endExchange e))
+
+(extend-protocol adapter/HandlerAdapter HttpServerExchange
+  (result-context,,, [e context] (handle-result-context context e))
+  (result-error,,,,, [e context throwable] (exchange/throw* e throwable))
+  (nio?,,,,,,,,,,,,, [e] (.isInIoThread e))
+  (blocking-call,,,, [e f] (.dispatch e ^Runnable f))
+  (async-call,,,,,,, [e f] (.dispatch e SameThreadExecutor/INSTANCE ^Runnable f)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn http-handler
+  ""
+  ([handlers] (http-handler handlers nil))
+  ([handlers {:keys [error-handler response-handler]}]
+   (let [context (cond-> {}
+                   error-handler (handler/set-error-handler error-handler)
+                   response-handler (handler/set-response-handler response-handler))]
+     (reify HttpHandler
+       (handleRequest [_ exchange]
+         (let [context (assoc context :request (request/create-request exchange) #_#_:start-time (System/nanoTime))]
+           (adapter/run-handlers exchange context handlers)))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
---
+++ src/user/server_spin_v3.clj
@@ -0,0 +1,120 @@
+(ns user.server-spin-v3
+  (:require [spin-undertow.handler-v3 :as spin-handler]
+            [spin.handler-v3 :as handler]
+            [undertow.server :as server])
+  (:import (io.undertow.server HttpHandler HttpServerExchange)
+           (io.undertow.util Methods)
+           (java.io InputStream)
+           (java.util.concurrent CompletableFuture)
+           (java.util.function Supplier)))
+
+(set! *warn-on-reflection* true)
+
+;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+#_(server/set-handler-fn-adapter spin-handler/http-handler)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn -test-middleware
+  [context]
+  (update context :response/headers
+          conj ["X-Test-Middleware" "was here"] ["X-Test" "middleware"]))
+
+(defn- t-name [] (.getName (Thread/currentThread)))
+
+(declare -request)
+
+(defn -test-handler
+  [{:keys [request] :as context}]
+  #_(def -request request)
+  #_(assoc context :response {:status 226 :body (str "instant - " (t-name)) :headers {"x-test" "handler"}}
+                   :response/status 404)
+  #_(if (= :post (request :method))
+      (delay (assoc context :response (str "delay - " (t-name) "\n\n"
+                                           (some-> ^InputStream (request :body) (.readAllBytes) (String.)))))
+      (assoc context :response (str "non-blocking - " (t-name))))
+  (if (= :post (request :method))
+    (handler/blocking
+      (fn [context] (assoc context :response (str "blocking - " (t-name) "\n\n"
+                                                  (some-> ^InputStream (request :body) (.readAllBytes) (String.))))))
+    (assoc context :response (str "non-blocking - " (t-name))))
+  #_(throw (ex-info "oops" {}))
+  #_(assoc context :response {:body (str "non-blocking - " (t-name))})
+  #_(delay (assoc context :response {:body (str "blocking - " (t-name))}))
+  #_(CompletableFuture/supplyAsync
+      (reify Supplier (get [_] (throw (ex-info "oops" {})))))
+  #_(CompletableFuture/supplyAsync
+      (reify Supplier (get [_] (assoc context :response {:body (str "async - " (t-name))})))))
+
+(defn -error-handler
+  [context throwable]
+  (assoc context :response {:status 500 :body (ex-message throwable)}))
+
+(declare ^HttpServerExchange -e)
+(def http-handler (reify HttpHandler (handleRequest [_ e]
+                                       (def ^HttpServerExchange -e e)
+                                       (-> (.getResponseSender e)
+                                           (.send "OK")))))
+
+(comment
+  (-request)
+  (meta (-request))
+  (-request :server-exchange)
+  (def ^HttpServerExchange -e (-request :server-exchange))
+  (-request :headers)
+  (-request :headers "Accept-Encoding")
+  (-request :headers "x-test")
+  (-request :headers "x-test" :many)
+  (-request :headers "x-test" false)
+  (.getFirst (.getRequestHeaders -e) "Accept-Encoding")
+  (.getRequestCookies -e)
+  (some-> (.getRequestCookie -e "JSESSIONID") (.getValue))
+  (-request :cookies)
+  (-request :cookies "JSESSIONID")
+  (-request :cookies "JSESSIONID" :data)
+  (first (.get (.getQueryParameters -e) "a"))
+  (.getQueryString -e)
+  (.peekFirst ^java.util.ArrayDeque (.get (.getQueryParameters -e) "a"))
+  (-request :query-string)
+  (-request :query-params)
+  (-request :query-params "a")
+  (-request :query-params "a" :many)
+  (-request :uri)
+  (-request :method)
+  (-request :method :raw)
+  (-request :request-method)
+  (.getRequestMethod -e)
+  (.equals Methods/GET (.getRequestMethod -e))
+  (-request :state)
+  (-request :state :k)
+  (-request :state :k :set! :v)
+  (-request :state :k :set! nil)
+  )
+
+(defn start-test-server
+  []
+  (-> {:port 8086
+       :handler (spin-handler/http-handler [-test-middleware -test-handler]
+                                           {:error-handler -error-handler})
+       #_#_:handler http-handler}
+      (server/start)))
+
+;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+(defonce server! (atom nil))
+
+(defn stop-server []
+  (swap! server! (fn [instance] (some-> instance server/stop))))
+
+(defn init-server []
+  (stop-server)
+  (reset! server! (start-test-server)))
+
+(init-server)
+
+(comment
+  (stop-server)
+  )
+
+;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
